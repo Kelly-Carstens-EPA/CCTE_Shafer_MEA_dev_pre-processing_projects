e750e513bf883cf574a15e6818f44fd5d6a36638
Amy Carpenter <carpenter.amy@epa.gov>
2020-04-23 16:50:16 -0400

    allow any number of h5 files per plate

--- MI_script_all.R
+++ MI_script_all.R
@@ -1,15 +1,15 @@
-# This script calculates the MI parameter
-# Notes:
-# - Must input all 4 DIV h5 files for each plate
+# This script was copied from NFA Spike List to mc0 R Scripts Bit Bucket repository, branch MI-related-updates on 04/17/2020 10:08am
+
+# This allows you to select the h5 files
+# then calls the functions to calculate the NMI
 
 library(rhdf5) # this is the library from Bio Conductor
 require('pracma')
 library('compiler')
-require('Matrix')
 require('gtools')
 
-h5Files<-choose.files(caption="Choose .h5 Files")
-h5Files = sort(h5Files)
+h5Files<-choose.files(caption="Select .h5 Files")
+h5Files <- sort(h5Files)
 
 ###################################################################################
 # USER INPUT
@@ -24,39 +24,37 @@ basepath = dirname(dirname(h5Files[1]))
 ###################################################################################
 
 # create All_MI directory
-suppressWarnings( dir.create(paste(basepath,'/All_MI',sep='') ) )
-mi.dir<-paste(basepath, "/All_MI",sep="")
+suppressWarnings( dir.create(paste(basepath,'/MI_test_results',sep='') ) )
+mi.dir<-paste(basepath, "/MI_test_results",sep="")
 
-# Use this function if all h5files are in same folder, and there is an h5File for each DIV 5,7,9,12 for each culture date
-streamline_one_folder = function(h5Files) {
-
-  N = length(h5Files)
-  prevdate = ""
+streamline_one_folder <- function(h5Files) {
   
-  for (i in seq(1,N,by=4)) {
+  # get a list of the date-plate combinations selected
+  date_plates <- unique( lapply(h5Files, function(x) strsplit(basename(x), split = "_")[[1]][2:3]) )
+  
+  for (date_plate in date_plates) {
     
-    filename = basename(h5Files[i])
-    date = strsplit(filename, split = "_")[[1]][2]
-    plate = strsplit(filename, split = "_")[[1]][3]
-    files = h5Files[grep(plate, h5Files)] # select all the DIV files for this plate
-    file_split<-split(files,1:length(files))
-    cat("Starting plate", plate, "at", as.character.Date(Sys.time()), "\n")
+    # select the files corresponding to the current plate
+    date <- date_plate[1]
+    plate <- date_plate[2]
+    plate_files <- h5Files[grepl(pattern = paste0(date,"_",plate,"_"), basename(h5Files))]
+    file_split<-split(plate_files,1:length(plate_files)) # legacy thing, haven't had the gumption to change to vector yet
+    
+    cat("Starting", date, plate, "at", as.character.Date(Sys.time()), "\n")
 
     #Begin MI analysis
     parsed_data = load.spikedata_final(333,file_split)
     MI_output<-list()
     MI_output<-nmi_wrapper(parsed_data)
 
-    # want to add col.names only if starting a new file for a new culture date
-    if (date == prevdate) {
-      write.table(MI_output, paste(mi.dir,"/MI_",date,".csv",sep=""),col.names=FALSE, append = TRUE, row.names=FALSE, sep=",")
-    } else {
-      write.table(MI_output, paste(mi.dir, "/MI_",date,".csv",sep=""), col.names=TRUE, append = FALSE, row.names=FALSE, sep=",")
-      prevdate = date
-    }
+    # save the result as csv file
+    file_name <- paste0(mi.dir,"/NMI_",date,"_",plate,".csv")
+    fwrite(MI_output, file = file_name,col.names=TRUE, append =FALSE, row.names=FALSE, sep=",")
+    
+    cat("\tCompleted",basename(file_name),"at",as.character.Date(Sys.time()),"\n")
+    
   }
-  print(paste0("Finished at ", as.character.Date(Sys.time())))
-  print("MI files are ready")
+  print(paste("MI files are ready in folder",mi.dir))
 }
 
 # Execute the function
--- spikeLoadRoutines.R
+++ spikeLoadRoutines.R
@@ -1,3 +1,11 @@
+# This script was copied from NFA Spike List to mc0 R Scripts Bit Bucket repository, branch MI-related-updates on 04/17/2020 9:57am
+# this script was edited in order to get the plate and data from the file name, instead of the h5file, as is done in the current master branch
+
+# This script assembles the spike data from the h5 files for the Mutual Information
+# This script is configured to extract the DIV values from the 4th tag in the h5 filename
+# div<-strsplit(file.name,split = "_")[[1]][4] (see below)
+# Change that line if needed
+
 require('rhdf5')
 require('gtools')
 
@@ -5,187 +13,147 @@ load.spikedata_final <- function(pseudoSamplingRate,fileName){
   # Loads spike data for a particular recording from h5 file.
   # Designed for [6,8] plate array with 4x4 MEAs in each well.
   # Returns a [6,8] data.frame of sparse [16,N] matrices. 
-  #fileName<-choose.files("choose the h5 files to be converted")  ### important that all files from same plate
+  
   x<-list()
   meta<-list()
   for (j in 1:length(fileName)){
     
-  # file <- h5read(fileName[[j]], 'r') # changing this to instead read each object from h5file individually
-  # other option: file <- H5Fopen(fileName[[j]])
-    # then access objects by file$spikes, etc. Then don't have to open as often...? see which is faster/more data efficient
-  test_h5<-try(h5read(fileName[[j]], '/Well'), silent=T) # checking capitalization of objects in fileName[[j]]
-  if (inherits(test_h5,"try-error")){
-  
-  spikes <- h5read(fileName[[j]], '/spikes')
-  well <- h5read(fileName[[j]], '/well')
-  names <- h5read(fileName[[j]], '/names')
-  sCount <- h5read(fileName[[j]], '/sCount')
-  treatment<-h5read(fileName[[j]], '/treatment')
-  dose<-h5read(fileName[[j]], '/dose')
-  units<-h5read(fileName[[j]], '/units')
-  #plate<-h5read(fileName[[j]], '/Plate.SN')
-  #date<-h5read(fileName[[j]], '/Experiment.Date')
-  #div<-h5read(fileName[[j]], '/DIV')
-  }  else{
-  spikes <- h5read(fileName[[j]], '/spikes')
-  well <- h5read(fileName[[j]], '/Well')
-  names <- h5read(fileName[[j]], '/names')
-  sCount <- h5read(fileName[[j]], '/sCount')
-  treatment<-h5read(fileName[[j]], '/Treatment')
-  dose<-h5read(fileName[[j]], '/Dose')
-  units<-h5read(fileName[[j]], '/Units')
-  plate<-h5read(fileName[[j]], '/Plate.SN')
-  date<-h5read(fileName[[j]], '/Experiment.Date')
-  }
-  file.name<-basename(fileName[[j]])
-  
-  t <- seq(min(spikes[]),max(spikes[]),by=1/pseudoSamplingRate)
-  t0 <- min(spikes[])
-  
-  spikeStreamArray <- as.list(matrix(nrow = 6, ncol = 8))
-  dim(spikeStreamArray) <- c(6,8)
-  # Initialize spiking array of zeros for each well
-  for (rr in 1:6) {
-    for (cc in 1:8) {
-      spikeStreamArray[[rr,cc]] <- matrix(0,nrow = 16, ncol = (length(t)))
+    test_h5<-try(h5read(fileName[[j]], '/Well'), silent=T) # checking capitalization/version of objects in fileName[[j]]
+    if (inherits(test_h5,"try-error")){
+      
+      spikes <- h5read(fileName[[j]], '/spikes')
+      well <- h5read(fileName[[j]], '/well')
+      names <- h5read(fileName[[j]], '/names')
+      sCount <- h5read(fileName[[j]], '/sCount')
+      treatment<-h5read(fileName[[j]], '/treatment')
+      dose<-h5read(fileName[[j]], '/dose')
+      units<-h5read(fileName[[j]], '/units')
+      # added the following 2 lines Mar 26, 2020 to work with changes in chem.info.3 in h5 file - 04/17/2020: removed for this script
+      # plate<-h5read(fileName[[j]], '/Plate.SN')
+      # date<-h5read(fileName[[j]], '/Experiment.Date')
+      # will get DIV from filename
+      #div<-h5read(fileName[[j]], '/DIV')
+    }  else{
+      spikes <- h5read(fileName[[j]], '/spikes')
+      well <- h5read(fileName[[j]], '/Well')
+      names <- h5read(fileName[[j]], '/names')
+      sCount <- h5read(fileName[[j]], '/sCount')
+      treatment<-h5read(fileName[[j]], '/Treatment')
+      dose<-h5read(fileName[[j]], '/Dose')
+      units<-h5read(fileName[[j]], '/Units')
+      plate<-h5read(fileName[[j]], '/Plate.SN')
+      date<-h5read(fileName[[j]], '/Experiment.Date')
     }
-  }
-  
-  # Populate each electrode in each well.
-  # Electrode conversion chart:
-  # #| 1  2  3  4
-  # --------------
-  # 1| 1  2  3  4
-  # 2| 5  6  7  8
-  # 3| 9  10 11 12
-  # 4| 13 14 15 16
-  
-  recallIndex = 1
-  
-  for (ii in 1:length(sCount[])) {
-    rr <- substr(names[ii],1,1) # the spiking electrode names
-    # Well row number:
-    # Convert character to ascii code value.. then normalize:
-    # [A,B,C,D,E,F] -> [65,66,67,68,69,70] -> '' - 64 ->[1,2,3,4,5,6]
-    rr <- as.integer(asc(rr))-64L
-    # Well column number:
-    cc <- strtoi(substr(names[ii],2,2))
-    # Electrode Number (see conversion chart above)
-    ee <- strtoi(substr(names[ii],4,4))-1
-    ee <- ee*4 + strtoi(substr(names[ii],5,5))
+    file.name<-basename(fileName[[j]])
     
-    # Now that we know where we are, we assign spikes at each time
-    # that they occur
-    #
-    # Retrieve the spiking times:
-    spikeTimes <- spikes[recallIndex:(recallIndex+sCount[ii]-1)]
-    # Convert to indices:
-    spikeTimes <- floor((spikeTimes-t0)*pseudoSamplingRate)+1 # convert time in s to num of 'samples'/bins after start
-    # Assign binary 1 values to array where spikes occur:
-    spikeStreamArray[[rr,cc]][ee,spikeTimes] <- 1 # error here: Error in .local(x, i, j, ..., value) : not-yet-implemented 'Matrix[<-' method
+    t <- seq(min(spikes[]),max(spikes[]),by=1/pseudoSamplingRate)
+    t0 <- min(spikes[])
     
-    # Update the recallIndex value so we crawl ahead through spikes
-    recallIndex <- recallIndex + sCount[ii]
-  }
-  x[[j]]<-spikeStreamArray
-  
-  wells_vector<-c()
-  trt_vector<-c()
-  dose_vector<-c()
-  unit_vector<-c()
-  plate_vector<-c()
-  date_vector<-c()
-  div_vector<-c()
-  file.name_vector<-c()
-  
-  if (inherits(test_h5,"try-error")){
-  
-  
-  for (i in 1:dim(well)){
-    wells_vector[i]<-well[i]
-    trt_vector[i]<-treatment[i]
-    dose_vector[i]<-dose[i]
-    unit_vector[i]<-units[i]
-    #plate_vector[i]<-plate[i]
-    #date_vector[i]<-date[i]
-    #div_vector[i]<-div[i]
-  }
-  # div<-substr(fileName[[j]],nchar(fileName[[j]])-11,nchar(fileName[[j]])-10)
-  # plate<-substr(fileName[[j]],nchar(fileName[[j]])-21,nchar(fileName[[j]])-13)
-  # date<-substr(fileName[[j]],nchar(fileName[[j]])-30,nchar(fileName[[j]])-23)
-  # Amy changed this 11/18/2019
-  # files with plate sn with fewer digits did not turn out the same
-  date<-strsplit(file.name,split = "_")[[1]][2]
-  plate<-strsplit(file.name,split = "_")[[1]][3]
-  div<-strsplit(file.name,split = "_")[[1]][4]
-  
-    
-  div_vector<-rep(div,length(wells_vector))
-  plate_vector<-rep(plate,length(wells_vector))
-  date_vector<-rep(date,length(wells_vector))
-  } else{
-    
-    for (i in 1:dim(well)){
-      wells_vector[i]<-well[i]
-      trt_vector[i]<-treatment[i]
-      dose_vector[i]<-dose[i]
-      unit_vector[i]<-units[i]
-      plate_vector[i]<-plate[i]
-      date_vector[i]<-date[i]
-      #div_vector[i]<-div[i]
+    spikeStreamArray <- as.list(matrix(nrow = 6, ncol = 8))
+    dim(spikeStreamArray) <- c(6,8)
+    # Initialize spiking array of zeros for each well
+    for (rr in 1:6) {
+      for (cc in 1:8) {
+        spikeStreamArray[[rr,cc]] <- matrix(0,nrow = 16, ncol = (length(t)))
+      }
     }
-    div<-substr(fileName[[j]],nchar(fileName[[j]])-11,nchar(fileName[[j]])-10)
-    #plate<-substr(fileName[[j]],nchar(fileName[[j]])-22,nchar(fileName[[j]])-13)
-    #date<-substr(fileName[[j]],nchar(fileName[[j]])-31,nchar(fileName[[j]])-23)
     
-    div_vector<-rep(div,length(wells_vector))
+    # Populate each electrode in each well.
+    # Electrode conversion chart:
+    # #| 1  2  3  4
+    # --------------
+    # 1| 1  2  3  4
+    # 2| 5  6  7  8
+    # 3| 9  10 11 12
+    # 4| 13 14 15 16
+    
+    recallIndex = 1
+    
+    for (ii in 1:length(sCount[])) {
+      rr <- substr(names[ii],1,1) # the spiking electrode names
+      # Well row number:
+      # Convert character to ascii code value.. then normalize:
+      # [A,B,C,D,E,F] -> [65,66,67,68,69,70] -> '' - 64 ->[1,2,3,4,5,6]
+      rr <- as.integer(asc(rr))-64L
+      # Well column number:
+      cc <- strtoi(substr(names[ii],2,2))
+      # Electrode Number (see conversion chart above)
+      ee <- strtoi(substr(names[ii],4,4))-1
+      ee <- ee*4 + strtoi(substr(names[ii],5,5))
+      
+      # Now that we know where we are, we assign spikes at each time
+      # that they occur
+      #
+      # Retrieve the spiking times:
+      spikeTimes <- spikes[recallIndex:(recallIndex+sCount[ii]-1)]
+      # Convert to indices:
+      spikeTimes <- floor((spikeTimes-t0)*pseudoSamplingRate)+1 # convert time in s to num of 'samples'/bins after start
+      # Assign binary 1 values to array where spikes occur:
+      spikeStreamArray[[rr,cc]][ee,spikeTimes] <- 1
+      
+      # Update the recallIndex value so we crawl ahead through spikes
+      recallIndex <- recallIndex + sCount[ii]
+    }
+    x[[j]]<-spikeStreamArray
+    
+    wells_vector<-c()
+    trt_vector<-c()
+    dose_vector<-c()
+    unit_vector<-c()
+    plate_vector<-c()
+    date_vector<-c()
+    div_vector<-c()
+    file.name_vector<-c()
+    
+    if (inherits(test_h5,"try-error")){
+      
+      
+      for (i in 1:dim(well)){
+        wells_vector[i]<-well[i]
+        trt_vector[i]<-treatment[i]
+        dose_vector[i]<-dose[i]
+        unit_vector[i]<-units[i]
+        # plate_vector[i]<-plate[i]
+        # date_vector[i]<-date[i]
+        #div_vector[i]<-div[i]
+      }
+      # add 04/17/2020 to collect date and plate from h5file name instead of from h5 body
+      date<-strsplit(file.name,split = "_")[[1]][2]
+      plate<-strsplit(file.name,split = "_")[[1]][3]
+      div<-strsplit(file.name,split = "_")[[1]][4]
+      
+      div_vector<-rep(div,length(wells_vector))
+      plate_vector<-rep(plate,length(wells_vector))
+      date_vector<-rep(date,length(wells_vector))
+      
+    } else{
+      
+      for (i in 1:dim(well)){
+        wells_vector[i]<-well[i]
+        trt_vector[i]<-treatment[i]
+        dose_vector[i]<-dose[i]
+        unit_vector[i]<-units[i]
+        plate_vector[i]<-plate[i]
+        date_vector[i]<-date[i]
+        #div_vector[i]<-div[i]
+      }
+      div<-substr(fileName[[j]],nchar(fileName[[j]])-11,nchar(fileName[[j]])-10)
+      #plate<-substr(fileName[[j]],nchar(fileName[[j]])-22,nchar(fileName[[j]])-13)
+      #date<-substr(fileName[[j]],nchar(fileName[[j]])-31,nchar(fileName[[j]])-23)
+      
+      div_vector<-rep(div,length(wells_vector))
+    }
+    file.name_vector<-rep(file.name, length(wells_vector))
+    
+    meta[[j]]<-cbind(date_vector,plate_vector,div_vector,wells_vector,trt_vector,dose_vector,unit_vector,file.name_vector)
+    
   }
-  file.name_vector<-rep(file.name, length(wells_vector))
   
-  meta[[j]]<-cbind(date_vector,plate_vector,div_vector,wells_vector,trt_vector,dose_vector,unit_vector,file.name_vector)
-  
+  # add the meta data to the end of x
+  L <- length(x)
+  for (i in 1:L) {
+    x[[L+i]] <- meta[[i]]
   }
-  
-  
-  
-  
-  
-  #date5<-substr(fileName[[1]],nchar(fileName[[1]])-30,nchar(fileName[[1]])-23)
-  #date7<-substr(fileName[[2]],nchar(fileName[[2]])-30,nchar(fileName[[2]])-23)
-  #date9<-substr(fileName[[3]],nchar(fileName[[3]])-30,nchar(fileName[[3]])-23)
-  #date12<-substr(fileName[[4]],nchar(fileName[[4]])-30,nchar(fileName[[4]])-23)
-  
-  #plate5<-substr(fileName[[1]],nchar(fileName[[1]])-21,nchar(fileName[[1]])-12)
-  #plate7<-substr(fileName[[2]],nchar(fileName[[2]])-21,nchar(fileName[[2]])-12)
-  #plate9<-substr(fileName[[3]],nchar(fileName[[3]])-21,nchar(fileName[[3]])-12)
-  #plate12<-substr(fileName[[4]],nchar(fileName[[4]])-21,nchar(fileName[[4]])-12)
-  
-  #div5<-rep(div5,nrow(meta))
-  #div7<-rep(div7,nrow(meta))
-  #div9<-rep(div9,nrow(meta))
-  #div12<-rep(div12,nrow(meta))
-  
-  #date5<-rep(date5,nrow(meta))
-  #date7<-rep(date7,nrow(meta))
-  #date9<-rep(date9,nrow(meta))
-  #date12<-rep(date12,nrow(meta))
-  
-  #plate5<-rep(plate5,nrow(meta))
-  #plate7<-rep(plate7,nrow(meta))
-  #plate9<-rep(plate9,nrow(meta))
-  #plate12<-rep(plate12,nrow(meta))
-  
-  meta5<-meta[[1]]
-  meta7<-meta[[2]]
-  meta9<-meta[[3]]
-  meta12<-meta[[4]]
-  
-  L=length(x)
-  x[[L+1]]<-meta5
-  x[[L+2]]<-meta7
-  x[[L+3]]<-meta9
-  x[[L+4]]<-meta12
-  
   
   # Return the collection of recordings across wells
   # for the given plate.
