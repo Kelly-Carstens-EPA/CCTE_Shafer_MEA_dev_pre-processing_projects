d2c5936c93a1b14682cf9bae41df3cd0dca89838
Amy Carpenter <carpenter.amy@epa.gov>
2020-09-02 10:59:33 -0400

    new run_me

--- R/run_me_RejectedCultures.R
+++ R/run_me_RejectedCultures.R
@@ -1,9 +1,7 @@
-rm(list=ls())
-graphics.off()
 ###################################################################################
 # USER INPUT
 ###################################################################################
-dataset_title <- "testpipeline2020" # the name for the current dataset, e.g. "name2020" (this should match the name of the folder under 'pre-process_mea_nfa_for_tcpl', e.g. 'Frank2017' or 'ToxCast2016')
+dataset_title <- "RejectedCultures" # the name for the current dataset, e.g. "name2020" (this should match the name of the folder under 'pre-process_mea_nfa_for_tcpl', e.g. 'Frank2017' or 'ToxCast2016')
 pause_between_steps <- TRUE # probs want to be true when you first run
 save_notes_graphs <- FALSE # Do this after have run thru once, to save a log of the steps. Set pause_between_steps to FALSE if saving notes and graphs for speed
 
@@ -35,6 +33,43 @@ if(save_notes_graphs) {
   graphics.off()
   pdf(file = file.path(root_output_dir, dataset_title, paste0(dataset_title,"_summary_plots_",as.character.Date(Sys.Date()),".pdf")))
 }
+
+# overwriting this to search for files myself
+# cultures_table <- as.data.table(read.csv("L:/Lab/NHEERL_MEA/Carpenter_Amy/pre-process_mea_nfa_for_tcpl/RejectedCultures/rejected_cultures_list.csv"))
+# file_names <- c()
+# for (culture in cultures_table$Culture) {
+#   cat("\n",culture,"\n", sep = "")
+#   culture.dir <- list.files(file.path(cultures_table[Culture == culture, Main_dir]), pattern = as.character(culture), full.names = T, include.dirs = T)
+#   plate.dirs <- grep("([0-9]){2,4}-([0-9]){1,4}$", list.dirs(culture.dir, recursive = F), val = T)
+#   if (length(plate.dirs)==0) {
+#     group.dirs <- grep("[Gg]roup",list.dirs(culture.dir, recursive = F), val = T)
+#     group.dirs <- group.dirs[grepl("[Gg]roup",basename(group.dirs))]
+#     plate.dirs <- unlist(lapply(group.dirs, function(x) grep("([0-9]){2,4}-([0-9]){1,4}$", list.dirs(x, recursive = F), val = T)))
+#   }
+#   
+#   # get calc files
+#   calc_files <- list.files(culture.dir, pattern = "Calculations", full.names = T)
+#   if (length(calc_files) > ceiling(length(plate.dirs)/3)) {
+#     calc_files <- choose.files(default = calc_files[1], caption = paste0("Select Calc files for ",culture))
+#   }
+#   for (plate.dir in plate.dirs) {
+#     cat(basename(plate.dir)," ")
+#     s_files <- list.files(path = plate.dir, pattern = "_spike_list", recursive = T, full.names = T)
+#     if (length(s_files) != 4) {
+#       cat(basename(s_files), sep ="\n")
+#       s_files <- choose.files(default = s_files[1], caption = "Select spike list files")
+#     }
+#     m_files <- list.files(path = file.path(plate.dir,"csv Files"), pattern = "_MaestroExperimentLog_Ontogeny")
+#     summary_files <- list.files(path = plate.dir, pattern = "Summary", recursive = T)
+#   }
+#   file_names <- c(file_names, unlist(sapply(Filter(function(x) length(get(x)) > 0, c("calc_files","s_files","m_files","summary_files")), get)))
+# }
+# length(file_names) # 40
+# nrow(cultures_table) # 14
+# source('gather_files_functions.R')
+# # then write it
+
+# ya know, let's just use the interface that I already made...
 
 # run the main steps
 source(file.path(scripts.dir, 'source_steps.R'))
