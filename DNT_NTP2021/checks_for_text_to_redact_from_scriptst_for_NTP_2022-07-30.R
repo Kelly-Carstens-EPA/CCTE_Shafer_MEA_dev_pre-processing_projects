ba069050be9f4ebf6d3754d15f8229c21ec14001
Amy Carpenter <Carpenter.Amy@epa.gov>
2023-06-15 20:56:55 -0400

    manually redacting phrases from head

--- DNT_NTP2021/checks_for_text_to_redact_from_scriptst_for_NTP_2022-07-30.R
+++ DNT_NTP2021/checks_for_text_to_redact_from_scriptst_for_NTP_2022-07-30.R
@@ -7,9 +7,9 @@ scripts.txt <- lapply(files, scan, what = character(), sep = '\n')
 which(unlist(lapply(scripts.txt, function(x) any(stri_detect(x, fixed = 'L:')))))
 # 5  8 10 11
 files[c( 5,  8, 10, 11)]
-# [1] "DNT_NTP2021/scripts_for_NTP/DNT_NTP2021_create_table_of_samples_to_rescreen_2022-07-27.R"                   
-# [2] "DNT_NTP2021/scripts_for_NTP/preliminary_run_me_DNT_NTP2021_2022-05-17.R"                                    
-# [3] "DNT_NTP2021/scripts_for_NTP/run_me_DNT_NTP2021.R"                                                           
+# [1] "DNT_NTP2021/scripts_for_NTP/DNT_NTP2021_create_table_of_samples_to_rescreen_2022-07-27.R"
+# [2] "DNT_NTP2021/scripts_for_NTP/preliminary_run_me_DNT_NTP2021_2022-05-17.R"
+# [3] "DNT_NTP2021/scripts_for_NTP/run_me_DNT_NTP2021.R"
 # [4] "DNT_NTP2021/scripts_for_NTP/update_wllq_table_for_original_runs_of_repeated_samples_to_exclude_2022-07-11.R"
 
 # made adjustments, recheck
@@ -19,14 +19,14 @@ which(unlist(lapply(scripts.txt, function(x) any(stri_detect(x, fixed = 'L:'))))
 # files 8  -> I know there is a commented out section wehre i reference RMySQL::MySQL() -> this is fine
 
 # check for any passwords
-which(unlist(lapply(scripts.txt, function(x) any(stri_detect(tolower(x), fixed = ***REMOVED***)))))
+which(unlist(lapply(scripts.txt, function(x) any(stri_detect(tolower(x), fixed = '**MANUALLY REDACTED**')))))
 # 8
 # ah -> I just have this in the comments in file 8
 # dbConnect(drv = RMySQL::MySQL(), user = "", pass = "", dbname='',host = "")
 # this is okay
 which(unlist(lapply(scripts.txt, function(x) any(stri_detect(tolower(x), fixed = 'user')))))
 files[c(8, 10)]
-# "DNT_NTP2021/scripts_for_NTP/preliminary_run_me_DNT_NTP2021_2022-05-17.R" "DNT_NTP2021/scripts_for_NTP/run_me_DNT_NTP2021.R" 
+# "DNT_NTP2021/scripts_for_NTP/preliminary_run_me_DNT_NTP2021_2022-05-17.R" "DNT_NTP2021/scripts_for_NTP/run_me_DNT_NTP2021.R"
 # Same line causing this flag for file 8
 # And in both , I have the "USER INPUT" section headings - > this is okay
 
@@ -44,8 +44,8 @@ which(unlist(lapply(scripts.txt, function(x) any(stri_detect(x, fixed = 'L:'))))
 # 5  8 10
 files[c( 5,  8, 10)]
 # [1] "DNT_NTP2021/scripts_for_NTP/DNT_NTP2021_create_table_of_samples_to_rescreen_2022-08-01.R"
-# [2] "DNT_NTP2021/scripts_for_NTP/preliminary_run_me_DNT_NTP2021_2022-05-17.R"                 
-# [3] "DNT_NTP2021/scripts_for_NTP/run_me_DNT_NTP2021.R"  
+# [2] "DNT_NTP2021/scripts_for_NTP/preliminary_run_me_DNT_NTP2021_2022-05-17.R"
+# [3] "DNT_NTP2021/scripts_for_NTP/run_me_DNT_NTP2021.R"
 
 # made adjustments, recheck
 scripts.txt <- lapply(files, scan, what = character(), sep = '\n')
@@ -55,13 +55,13 @@ files[c(8)]
 # files 8  -> I know there is a commented out section wehre i reference RMySQL::MySQL() -> this is fine
 
 # check for any passwords
-which(unlist(lapply(scripts.txt, function(x) any(stri_detect(tolower(x), fixed = ***REMOVED***)))))
+which(unlist(lapply(scripts.txt, function(x) any(stri_detect(tolower(x), fixed = '**MANUALLY REDACTED**')))))
 # 8
 # ah -> I just have this in the comments in file 8
 # dbConnect(drv = RMySQL::MySQL(), user = "", pass = "", dbname='',host = "")
 # this is okay
 which(unlist(lapply(scripts.txt, function(x) any(stri_detect(tolower(x), fixed = 'user')))))
 files[c(8, 10)]
-# "DNT_NTP2021/scripts_for_NTP/preliminary_run_me_DNT_NTP2021_2022-05-17.R" "DNT_NTP2021/scripts_for_NTP/run_me_DNT_NTP2021.R" 
+# "DNT_NTP2021/scripts_for_NTP/preliminary_run_me_DNT_NTP2021_2022-05-17.R" "DNT_NTP2021/scripts_for_NTP/run_me_DNT_NTP2021.R"
 # Same line causing this flag for file 8
 # And in both , I have the "USER INPUT" section headings - > this is okay
--- investigations/check_for_phrases_in_scripts_2022-01-06.R
+++ investigations/check_for_phrases_in_scripts_2022-01-06.R
@@ -4,7 +4,7 @@
 
 check.phrases <- c('L:/Lab',
                    'MySQL',
-                   ***REMOVED***,
+                   '**MANUALLY REDACTED**',
                    'dbConnect',
                    'tcplConf')
 
@@ -12,21 +12,21 @@ files <- list.files(path = 'L:/Lab/NHEERL_MEA/Carpenter_Amy/pre-process_mea_nfa_
 
 
 for (filei in files) {
-  
+
   cat(basename(filei), '\n')
   script_txt <- scan(file = filei, what = character(), sep = '\n', quiet = T)
-  
+
   # check for any matches with the check.phrases
   res <- sapply(check.phrases, function(stri) grepl(pattern = stri, script_txt))
-  
+
   # If any matches, spit them out
   if (any(unlist(lapply(res, any)))) {
     print(which(res == TRUE, arr.ind = T))
   }
 }
 
-# burst_parameter_to_AUC.R 
-# confirm_concs.R 
+# burst_parameter_to_AUC.R
+# confirm_concs.R
 # row col
 # [1,]   3   2
 # [2,]  13   2
@@ -34,22 +34,22 @@ for (filei in files) {
 # [4,]  13   4
 # This is a real issue
 
-# create_burst_ont_Data.R 
-# create_ont_csv.R 
-# cytotox_prep06.R 
+# create_burst_ont_Data.R
+# create_ont_csv.R
+# cytotox_prep06.R
 # row col
 # [1,] 315   3
 # using the word pass, not short for password
 
-# dataset_checks.R 
-# DIV-interpolation-functions.R 
-# estimate_missing_DIV.R 
-# gather_files_functions.R 
+# dataset_checks.R
+# DIV-interpolation-functions.R
+# estimate_missing_DIV.R
+# gather_files_functions.R
 # row col
 # [1,]  54   3
 # using the word pass, not short for password
 
-# get_spike_list_files.R 
+# get_spike_list_files.R
 # row col
 # [1,]  76   1
 # [2,]  77   1
@@ -57,15 +57,15 @@ for (filei in files) {
 # Just removed specific references to file paths
 # using the word pass, not short for password
 
-# h5_conversion.R 
-# local.corr.all.ont.ae.filter.R 
-# MI_script_all.R 
-# nmi_wrapper.R 
-# nmi2_final.R 
-# normalize_auc_summary.R 
+# h5_conversion.R
+# local.corr.all.ont.ae.filter.R
+# MI_script_all.R
+# nmi_wrapper.R
+# nmi2_final.R
+# normalize_auc_summary.R
 
 # Not going to include any of the run_me's, just the templates:
-# run_me_Brown2014.R 
+# run_me_Brown2014.R
 # row col
 # [1,]  10   1
 # [2,]  11   1
@@ -76,7 +76,7 @@ for (filei in files) {
 # [7,] 233   2
 # [8,] 233   3
 # [9,] 233   4
-# run_me_DNTGF2019.R 
+# run_me_DNTGF2019.R
 # row col
 # [1,]  10   1
 # [2,]  12   1
@@ -95,69 +95,69 @@ for (filei in files) {
 # [15,] 423   1
 # [16,] 445   1
 # [17,] 473   1
-# run_me_Example2020.R 
+# run_me_Example2020.R
 # row col
 # [1,]  14   1
 # [2,]  16   1
 # [3,]  17   1
-# run_me_Frank2017.R 
+# run_me_Frank2017.R
 # row col
 # [1,]  12   1
 # [2,]  13   1
 # [3,] 182   1
-# run_me_NTP91.R 
+# run_me_NTP91.R
 # row col
 # [1,]  10   1
 # [2,]  12   1
 # [3,]  13   1
-# run_me_OPP2015.R 
+# run_me_OPP2015.R
 # row col
 # [1,]  10   1
 # [2,]  11   1
-# run_me_PFAS2018.R 
+# run_me_PFAS2018.R
 # row col
 # [1,]  10   1
 # [2,]  12   1
 # [3,]  13   1
-# run_me_PFAS2019.R 
+# run_me_PFAS2019.R
 # row col
 # [1,]  12   1
 # [2,]  13   1
 # [3,] 156   1
 # [4,] 159   1
 # [5,] 258   1
-# run_me_RejectedCultures.R 
+# run_me_RejectedCultures.R
 # row col
 # [1,]  10   1
 # [2,]  11   1
-# run_me_SPS_PFAS2019.R 
+# run_me_SPS_PFAS2019.R
 # row col
 # [1,]  13   1
 # [2,]  14   1
 # [3,] 179   1
-# run_me_template.R 
+# run_me_template.R
 # row col
 # [1,]  12   1
 # [2,]  13   1
 # Just removed these
 
-# run_me_ToxCast2016.R 
+# run_me_ToxCast2016.R
 # row col
 # [1,]  10   1
 # [2,]  11   1
 # [3,]  96   2
 # [4,]  96   3
 # [5,]  96   4
-# run_me_wide_template.R 
+# run_me_wide_template.R
 # row col
 # [1,]  10   1
 # [2,]  11   1
 # Just removed these
 
-# source_steps.R 
-# spike_list_functions.R 
-# spikeLoadRoutines.R 
-# tcpl_MEA_dev_AUC.R 
+# source_steps.R
+# spike_list_functions.R
+# spikeLoadRoutines.R
+# tcpl_MEA_dev_AUC.R
 #      row col
 # [1,]   9   1
 # [2,]  10   1
@@ -173,13 +173,13 @@ check.phrases <- c('Created with the script get_spike_list_files')
 files <- list.files(path = 'L:/Lab/NHEERL_MEA/Carpenter_Amy/pre-process_mea_nfa_for_tcpl', pattern = '\\.txt', full.names = T, recursive = T)
 
 for (filei in files) {
-  
+
   cat(basename(filei), '\n')
   script_txt <- scan(file = filei, what = character(), sep = '\n', quiet = T)
-  
+
   # check for any matches with teh check.phrases
   res <- sapply(check.phrases, function(stri) grepl(pattern = stri, script_txt))
-  
+
   # If any matches, spit them out
   if (any(unlist(lapply(res, any)))) {
     print(which(res == TRUE, arr.ind = T))
